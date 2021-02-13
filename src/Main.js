295b6e17e04cb90a8535837911905db0a6133796
Liam Becker <liambecker28@gmail.com>
2021-02-13 16:30:04 -0500

    Pull api calls into helper functions

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
-import axios from 'axios';
 
-import { getSeasonName, getScore } from './utils';
+// Styles
 import './Main.css';
 
-const API_URL = 'https://rpdr-party-games.herokuapp.com/api';
+// Utilities
+import { getSeasonName, getScore } from './utils';
+import api from './api';
 
 class Main extends Component {
   constructor(props) {
@@ -24,55 +25,83 @@ class Main extends Component {
 
   /** Use API call to get users, and get result based on season. */
   async componentDidMount() {
-    const users = (await axios.get(`${API_URL}/users`)).data;
-    const queens = (await axios.get(`${API_URL}/queens`)).data;
-    const seasons = (await axios.get(`${API_URL}/seasons`)).data;
+    const users = await api.getAllUsers();
+    const queens = await api.getAllQueens();
+    const seasons = await api.getAllSeasons();
     const selectedSeason = seasons[0];
-    const result = (await axios.get(`${API_URL}/seasons/${selectedSeason.id}`)).data.contestants;
-    const predictions = (await axios.get(`${API_URL}/predictions/season/${selectedSeason.id}`)).data;
+    const result = await api.getSeasonFromId(selectedSeason.id);
+    const predictions = await api.getPredictionsBySeason(selectedSeason.id);
 
-    this.setState({ users, queens, seasons, selectedSeason, result, predictions });
+    this.setState({
+      users,
+      queens,
+      seasons,
+      selectedSeason,
+      result,
+      predictions,
+    });
   }
 
   /** Update the draft list and scoreboard for the selected season. */
   async updatePage(event) {
     event.preventDefault();
-    
-    const selectedIndex = event.target.options.selectedIndex;
-    const seasonKey = event.target.options[selectedIndex].getAttribute('seasonkey').split(":");
-    const selectedSeason = this.state.seasons.find(
-      season => season.series === seasonKey[0] && season.num === parseInt(seasonKey[1]));
 
-    const result = (await axios.get(`${API_URL}/seasons/${selectedSeason.id}`)).data.contestants;
-    const predictions = (await axios.get(`${API_URL}/predictions/season/${selectedSeason.id}`)).data;
+    const selectedIndex = event.target.options.selectedIndex;
+    const seasonKey = event.target.options[selectedIndex]
+      .getAttribute('seasonkey')
+      .split(':');
+    const selectedSeason = this.state.seasons.find(
+      (season) =>
+        season.series === seasonKey[0] && season.num === parseInt(seasonKey[1])
+    );
+
+    const result = await api.getSeasonFromId(selectedSeason.id);
+    const predictions = await api.getPredictionsBySeason(selectedSeason.id);
 
     this.setState({ selectedSeason, result, predictions });
   }
 
   render() {
-  
     return (
       <div>
-        <div align="center" >
+        <div align='center'>
           <select onChange={this.updatePage}>
-            { 
-              this.state.seasons.map(season => <option key={season.series + ":" + season.num} seasonkey={season.series + ":" + season.num}>{getSeasonName(season.series)} Season {season.num}</option>)
-            }
+            {this.state.seasons.map((season) => (
+              <option
+                key={season.series + ':' + season.num}
+                seasonkey={season.series + ':' + season.num}
+              >
+                {getSeasonName(season.series)} Season {season.num}
+              </option>
+            ))}
           </select>
         </div>
         <div>
-          <ListGroup className="scoreboard">
+          <ListGroup className='scoreboard'>
             <h4>Scoreboard</h4>
-            {this.state.users.map((user) => <ListGroup.Item key={user.id} >{user.name} - {getScore()}</ListGroup.Item>)}
+            {this.state.users.map((user) => (
+              <ListGroup.Item key={user.id}>
+                {user.name} - {getScore()}
+              </ListGroup.Item>
+            ))}
           </ListGroup>
-          <div className="line-ups">
+          <div className='line-ups'>
             <div>
               <h4>Results</h4>
-              <Draft draft={this.state.result} queens={this.state.queens}/>
+              <Draft draft={this.state.result} queens={this.state.queens} />
             </div>
             <div>
-              <h4 >All Users</h4>
-              {this.state.users.map((user) => <UserEntry key={user.id} user={user} draft={this.state.predictions.filter(draft => draft.user_id === user.id)} queens={this.state.queens} />)}
+              <h4>All Users</h4>
+              {this.state.users.map((user) => (
+                <UserEntry
+                  key={user.id}
+                  user={user}
+                  draft={this.state.predictions.filter(
+                    (draft) => draft.user_id === user.id
+                  )}
+                  queens={this.state.queens}
+                />
+              ))}
             </div>
           </div>
         </div>
@@ -85,23 +114,29 @@ function UserEntry(props) {
   return (
     <div>
       <h6>{props.user.name}</h6>
-      <Draft draft={props.draft} queens={props.queens}/>
+      <Draft draft={props.draft} queens={props.queens} />
     </div>
-  )
+  );
 }
 
 function Draft(props) {
   const sortedPredictionList = props.draft
-    .filter(entry => entry.placement !== null)
+    .filter((entry) => entry.placement !== null)
     .sort((a, b) => b.placement - a.placement)
-    .map(prediction => props.queens.find((queen) => queen.id === prediction.queen_id));
+    .map((prediction) =>
+      props.queens.find((queen) => queen.id === prediction.queen_id)
+    );
 
   return (
     <ListGroup horizontal>
-      {sortedPredictionList.map(queen => {
-        return <ListGroup.Item className="prediction-item" key={queen.id}>{queen.name}</ListGroup.Item>
+      {sortedPredictionList.map((queen) => {
+        return (
+          <ListGroup.Item className='prediction-item' key={queen.id}>
+            {queen.name}
+          </ListGroup.Item>
+        );
       })}
-    </ListGroup> 
+    </ListGroup>
   );
 }
 
