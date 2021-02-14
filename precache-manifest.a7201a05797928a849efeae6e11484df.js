ea319571385fa7f75ac36b5b3bbdf57f99aaf688
Perry Healy <perryhealy@gmail.com>
2021-02-14 11:55:36 -0500

    Updates

---
+++ precache-manifest.a7201a05797928a849efeae6e11484df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4870fa709c47def9be40276e990f9e",
+    "url": "/long-hauls/index.html"
+  },
+  {
+    "revision": "ea1ec33ad4a377fadff9",
+    "url": "/long-hauls/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "942652bb7fd79ede07ba",
+    "url": "/long-hauls/static/css/main.03e30f0c.chunk.css"
+  },
+  {
+    "revision": "ea1ec33ad4a377fadff9",
+    "url": "/long-hauls/static/js/2.35349327.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/long-hauls/static/js/2.35349327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942652bb7fd79ede07ba",
+    "url": "/long-hauls/static/js/main.8595972e.chunk.js"
+  },
+  {
+    "revision": "968c3a2bfd7fd204cb98",
+    "url": "/long-hauls/static/js/runtime-main.678e140b.js"
+  }
+]);
